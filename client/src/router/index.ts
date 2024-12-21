ad56086e8fc487b217448fea4c631abba7aeac5c
Trenttzzz <azzafarichi2@gmail.com>
2024-12-21 11:05:43 +0700

    ui form peminjaman don

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -3,6 +3,8 @@ import LoginPage from '@/views/LoginPage.vue'
 import AdminPage from '@/views/AdminPage.vue'
 import EditItem from '@/components/AdminPage/EditItem.vue'
 import OperatorPage from '@/views/OperatorPage.vue'
+import SummaryPage from '@/views/SummaryPage.vue'  // You'll need to create this component
+import BorrowForm from '@/components/OperatorPage/BorrowForm.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +29,21 @@ const router = createRouter({
       path: '/admin/all-operator',
       name: 'all-operator',
       component: OperatorPage,
+    },
+    {
+      path: '/operator',
+      name: 'operator',
+      component: OperatorPage // ganti jadi home page nya operator
+    },
+    {
+      path: '/operator/book-equipment',
+      name: 'book-equipment',
+      component: BorrowForm
+    },
+    {
+      path: '/summary',
+      name: 'summary',
+      component: SummaryPage
     }
   ],
 })
